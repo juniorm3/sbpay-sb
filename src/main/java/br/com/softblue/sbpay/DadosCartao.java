b75ab3d3107108fe419089b326f276dc8f3f2bd4
Mario <mario.martins@sonda.com>
2020-09-07 12:10:44 -0300

    14.10 (Prática): Criando um web service de pagamento - Parte 2

--- src/main/java/br/com/softblue/sbpay/DadosCartao.java
+++ src/main/java/br/com/softblue/sbpay/DadosCartao.java
@@ -5,14 +5,15 @@ import javax.validation.constraints.Pattern;
 public class DadosCartao {
 
 	@Pattern(regexp = "\\d{16}", message = "O número do cartão é inválido")
-	private String numeroCartao;
-
-	public String getNumeroCartao() {
-		return numeroCartao;
+	private String numCartao;
+	
+	public String getNumCartao() {
+		return numCartao;
+	}
+	
+	public void setNumCartao(String numCartao) {
+		this.numCartao = numCartao;
 	}
 
-	public void setNumeroCartao(String numeroCartao) {
-		this.numeroCartao = numeroCartao;
-	}
 
 }
